6241f9b70d5c39fd02af0f4fff68ac60c00f9c9e
Salmane <elmourtajisalmane@gmail.com>
2023-11-01 15:34:24 +0100

    Create README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# mapxplorer
+A JavaFX-powered desktop app for exploring maps, finding nearby places, and uncovering insightful location statistics with OpenStreetMap data."
--- src/main/java/com/mapxplorer/MapXplorerApp.java
+++ src/main/java/com/mapxplorer/MapXplorerApp.java
@@ -11,8 +11,8 @@ public class MapXplorerApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(MapXplorerApp.class.getResource("view/mainView.fxml"));
-        Scene scene = new Scene(loader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(loader.load(), 500, 300);
+        stage.setTitle("Mapxplorer");
         stage.setScene(scene);
         stage.show();
     }
