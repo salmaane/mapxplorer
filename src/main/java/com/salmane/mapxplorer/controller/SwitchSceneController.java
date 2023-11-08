e3539d5902f57fd8edb26419786f9155836f3e79
Salmane <elmourtajisalmane@gmail.com>
2023-11-08 17:51:59 +0100

    created a sidebar in home scene with icons and tooltips

--- src/main/java/com/salmane/mapxplorer/controller/SwitchSceneController.java
+++ src/main/java/com/salmane/mapxplorer/controller/SwitchSceneController.java
@@ -2,9 +2,11 @@ package com.salmane.mapxplorer.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,7 +23,24 @@ public class SwitchSceneController {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        centerStage();
         stage.show();
+    }
+
+    private void centerStage() {
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        double screenWidth = bounds.getWidth();
+        double screenHeight = bounds.getHeight();
+        double stageWidth = stage.getWidth();
+        double stageHeight = stage.getHeight();
+
+        double newX = (screenWidth - stageWidth) / 2;
+        double newY = (screenHeight - stageHeight) / 2;
+
+        stage.setX(newX);
+        stage.setY(newY);
     }
 
 }
