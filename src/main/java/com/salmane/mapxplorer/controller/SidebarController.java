8fbce4805d297d5a79b054e6fe0f1afd6b04be03
Salmane <elmourtajisalmane@gmail.com>
2023-11-10 11:02:25 +0100

    redesigned sidebar to add collapse and expand feature
    by adding an event handler in sidebar Controller

--- src/main/java/com/salmane/mapxplorer/controller/SidebarController.java
+++ src/main/java/com/salmane/mapxplorer/controller/SidebarController.java
@@ -1,8 +1,10 @@
 package com.salmane.mapxplorer.controller;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class SidebarController {
@@ -12,6 +14,19 @@ public class SidebarController {
     public FontAwesomeIconView offlineIcon;
     public FontAwesomeIconView aboutIcon;
     public FontAwesomeIconView menuIcon;
+    private boolean collapsed = false;
+
+    public void handleToggleMenu(ActionEvent event) {
+        if(collapsed) {
+            sidebar.setPrefWidth(230);
+            sidebar.setClip(new Rectangle(230, 746));
+            collapsed = false;
+            return;
+        }
+        sidebar.setPrefWidth(43);
+        sidebar.setClip(new Rectangle(43, 746));
+        collapsed = true;
+    }
 
     public void initialize() {
         Tooltip homeTooltip = new Tooltip("Home");
