cf6f9ae5e3a8db10c13a48ad578eac93a83f535f
Salmane <elmourtajisalmane@gmail.com>
2023-11-16 22:26:12 +0100

    added leaflet layer to webview component using cdn links in javascript

--- src/main/java/com/salmane/mapxplorer/controller/LeafletMapController.java
+++ src/main/java/com/salmane/mapxplorer/controller/LeafletMapController.java
@@ -1,11 +1,30 @@
 package com.salmane.mapxplorer.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.HBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class LeafletMapController {
+
     @FXML
-    public HBox mainContainer;
+    public WebView webview;
+    public WebEngine engine;
+
+    public void initialize() {
+        engine = webview.getEngine();
+        engine.load(String.valueOf(getClass().getResource("/com/salmane/mapxplorer/javascript/leaflet.html")));
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+//        webEngine.load(String.valueOf(getClass().getResource("/com/salmane/mapxplorer/javascript/leaflet.html")));
